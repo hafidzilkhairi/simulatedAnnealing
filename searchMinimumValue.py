e62f2c7556e0b7ca9eaa33130fcb0f196e56f9f1
Hafidzil Khairi <hafidzilkhairi00@gmail.com>
2018-09-18 00:53:12 +0700

    End of Project

--- searchMinimumValue.py
+++ searchMinimumValue.py
@@ -1,5 +1,6 @@
 import math
 import random
+import time
 def hasilOperasi(x,y):
     sinus = math.sin(x)
     cosinus = math.cos(y)
@@ -12,3 +13,29 @@ def hasilOperasi(x,y):
 def hasilRandom():
     return (-10 + 20 * random.uniform(0,1))
 
+
+def start():
+    T = 1000000
+    x = hasilRandom()
+    y = hasilRandom()
+    solusi = hasilOperasi(x, y)
+    T = 1000000
+    while(T>1):
+        xNew = hasilRandom()
+        yNew = hasilRandom()
+        solusiNew = hasilOperasi(xNew, yNew)
+        delta = solusiNew - solusi
+        if(delta < 0):
+            x = xNew
+            y = yNew
+            solusi = solusiNew
+            print("Masuk if")
+        elif ((math.exp(-delta/T) > (random.uniform(0, 1)))):
+            x = xNew
+            y = yNew
+            solusi = solusiNew
+            T = 0.6 * T
+            print("Masuk else")
+    print(solusi)
+
+start()
